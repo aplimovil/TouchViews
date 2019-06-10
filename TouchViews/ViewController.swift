c7f6770a4164f0a0788e761f84a118a1744285a7
aplimovil <fomarti@unicauca.edu.co>
2019-06-10 11:08:03 -0500

    Initial Commit

---
+++ TouchViews/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  TouchViews
+//
+//  Created by Craig Grummitt on 21/04/2016.
+//  Copyright © 2016 Craig Grummitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set up views
+        let margin:CGFloat = 20
+        let halfWidth = view.frame.width / 2
+        let halfHeight = view.frame.height / 2
+        
+        let viewA = UIView(frame: CGRect(
+            x: margin,
+            y: margin,
+            width: halfWidth,
+            height: halfWidth
+            ))
+        viewA.backgroundColor = UIColor.random
+        self.view.addSubview(viewA)
+        
+        let viewB = UIView(frame: CGRect(
+            x: view.frame.width - halfWidth - margin,
+            y: (view.frame.height / 2) - halfWidth,
+            width: halfWidth,
+            height: halfWidth
+            ))
+        viewB.backgroundColor = UIColor.random
+        self.view.addSubview(viewB)
+        
+        let viewC = UIView(frame: CGRect(
+            x: margin,
+            y: halfHeight + margin,
+            width: view.frame.width - (margin * 2),
+            height: halfHeight - (margin * 2)
+            ))
+        viewC.backgroundColor = UIColor.random
+        self.view.addSubview(viewC)
+        
+        let viewD = UIView(frame: CGRect(
+            x: (view.frame.width / 2) - margin,
+            y: margin,
+            width: halfWidth - margin * 2,
+            height: viewC.frame.height - (margin * 2)
+            ))
+        viewD.backgroundColor = UIColor.random
+        viewC.addSubview(viewD)                     // View D is a subview of view C
+        
+        
+        self.view.backgroundColor = UIColor.random
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
